ff2ceab6f98885e6bf48be563597f2252d06935c
Pierre Le Gall <pierre@legall.im>
2019-01-07 10:41:35 +0100

    Fix "export 'SET_USER' was not found in 'authentication.mutations'"

--- frontend/src/store/modules/authentication/authentication.mutations.js
+++ frontend/src/store/modules/authentication/authentication.mutations.js
@@ -1,5 +1,7 @@
+export const SET_USER = 'SET USER';
 export const SET_USER_INFOS = 'SET USER INFOS';
 
 export default {
+  [SET_USER]: (state, value) => (state.user = value),
   [SET_USER_INFOS]: (state, value) => (state.userInfos = value)
 };
