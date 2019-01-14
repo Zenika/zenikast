e0c479dce9ed7cce0b80e5a0e8cd21031294a23e
Pierre Le Gall <pierre@legall.im>
2019-01-14 16:51:20 +0100

    Config added for the dev environment named zenikast-dev

--- frontend/src/firebase/index.js
+++ frontend/src/firebase/index.js
@@ -6,14 +6,23 @@ import 'firebase/auth';
 import 'firebase/firestore';
 import 'firebase/storage';
 
-const config = {
-  apiKey: 'AIzaSyBcZWD1CGGw2nRyHC_ipPiOK1szWb52Al8',
-  authDomain: 'zenikast-a6114.firebaseapp.com',
-  databaseURL: 'https://zenikast-a6114.firebaseio.com',
-  projectId: 'zenikast-a6114',
-  storageBucket: 'zenikast-a6114.appspot.com',
-  messagingSenderId: '863526141805'
-};
+const config = {};
+
+if (process.env.NODE_ENV === 'production') {
+  config.apiKey = 'AIzaSyBcZWD1CGGw2nRyHC_ipPiOK1szWb52Al8';
+  config.authDomain = 'zenikast-a6114.firebaseapp.com';
+  config.databaseURL = 'https://zenikast-a6114.firebaseio.com';
+  config.projectId = 'zenikast-a6114';
+  config.storageBucket = 'zenikast-a6114.appspot.com';
+  config.messagingSenderId = '863526141805';
+} else {
+  config.apiKey = 'AIzaSyDKb67XEkQuyZK9gQqGfALbOFOAFIUUj-E';
+  config.authDomain = 'zenikast-dev.firebaseapp.com';
+  config.databaseURL = 'https://zenikast-dev.firebaseio.com';
+  config.projectId = 'zenikast-dev';
+  config.storageBucket = 'zenikast-dev.appspot.com';
+  config.messagingSenderId = '785046092748';
+}
 
 /* Firebase init */
 firebase.initializeApp(config);
